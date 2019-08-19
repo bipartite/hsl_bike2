eb19b31f6889131e1b62e489cae9ebe0a144261c
jee <kepoju@hotmail.com>
2019-08-19 13:08:52 +0300

    elastic template, fixes

--- fetch_bike_data.py
+++ fetch_bike_data.py
@@ -6,9 +6,11 @@ import sys
 import os
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
+from elasticsearch_dsl import GeoPoint
 
 es_client = Elasticsearch(http_compress=True)
 
+GeoPoint.to_dict = lambda self: {'lat': self.lat, 'lon': self.lon}
 
 def get_data():
     url = 'http://api.citybik.es/v2/networks/citybikes-helsinki'
@@ -18,6 +20,7 @@ def get_data():
         df = pd.DataFrame.from_dict(city_bikes['network']['stations'])
         df = df[1:].drop('extra', axis=1) #Discard first row, drop 'extra' column
         df['timestamp'] = df['timestamp'].apply(safe_date)
+        df['location'] = df[['latitude', 'longitude']].apply(create_geopoint, axis=1)
         return df
     except requests.exceptions.ConnectionError:
         print('{}: Connection failed'.format(stamp))
@@ -29,11 +32,16 @@ def safe_date(date_value):
             else  datetime(1970,1,1,0,0)
     )
 
+def create_geopoint(row):
+    return {'lat': row['latitude'], 'lon': row['longitude']}
+
 def doc_generator(df):
     df_iter = df.iterrows()
+    ts = time.time()
+    stamp = datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H')
     for index, document in df_iter:
         yield {
-                "_index": 'your_index',
+                "_index": 'hsl_bike_{0}'.format(stamp),
                 "_type": "_doc",
                 "_id" : f"{document['id']}",
                 "_source": document.to_dict(),
